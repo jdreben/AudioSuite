cd0e4d9dc4fa488432940b1f020e32bf2cc76d92
jdreben <James@Dreben.com>
2014-04-20 17:59:22 -0400

    MP3Decoder

--- src/com/AudioSuite/MP3Decoder.java
+++ src/com/AudioSuite/MP3Decoder.java
@@ -1,10 +1,24 @@
 package com.AudioSuite;
 
+import java.io.FileInputStream;
+
 import javazoom.jl.decoder.*;
 
 public class MP3Decoder {
 
 	public static void main(String args[]) {
-		Decoder betsy = new Decoder();
+		try {
+		Bitstream bitStream = new Bitstream(new FileInputStream("/Users/James/Documents/workspace/Audio/Resources/believe.wav"));
+
+		while(true){
+		    Decoder decoder = new Decoder();
+		    SampleBuffer samples = (SampleBuffer)decoder.decodeFrame(bitStream.readFrame(), bitStream); //returns the next 2304 samples
+		    System.out.println(samples.getBuffer());
+		    bitStream.closeFrame();
+		    //do whatever with your samples
+		}
+		} catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 }
