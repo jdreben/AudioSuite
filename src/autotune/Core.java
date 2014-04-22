211243fb68b4a1c1188f71ad6ba61faec8c3ff80
jdreben <James@Dreben.com>
2014-04-21 21:45:10 -0400

    need to deal with short v. double interpretation issue

--- src/autotune/Core.java
+++ src/autotune/Core.java
@@ -1,19 +1,28 @@
 package autotune;
 
 import java.io.FileInputStream;
-
 import javazoom.jl.decoder.*;
+import javazoom.jl.player.*;
+
 public class Core {
 
 	public static void main (String args[]) {
+		//sanity check, plays music
+		/*try {
+			Player playa = new Player(new FileInputStream("/Users/James/Documents/workspace/Audio/Resources/Canon.mp3"));
+			playa.play();
+		} catch (Exception e) { System.out.println(e); } */
+		
 		try {
-			Bitstream bitStream = new Bitstream(new FileInputStream("/Users/James/Documents/workspace/Audio/Resources/believe.m4a"));
+			Bitstream bitStream = new Bitstream(new FileInputStream("/Users/James/Documents/workspace/Audio/Resources/Canon.mp3"));
 			boolean eof = false;
 			while(!eof) {
 				Header head =  bitStream.readFrame();
 				if (head == null) { eof=true; }
 				Decoder dec = new Decoder();
+				
 				SampleBuffer samples = (SampleBuffer)dec.decodeFrame(head, bitStream); 
+				
 				for (int i=0;i<samples.getBufferLength();i++) {
 				System.out.print("before mod : "); System.out.print(samples.getBuffer()[i]+"\n");
 				}
@@ -21,15 +30,14 @@ public class Core {
 				Autotune tune = new Autotune(head, samples);
 				SampleBuffer samp = (SampleBuffer)tune.getTuned();    
 
-				System.out.print("after mod : "); System.out.print(samp.getBuffer()[1]+"\n");
+				System.out.print("after mod : "); System.out.print(samp.getBuffer()[1]+"\n"); 
 				/*
 				 *  unsure how to convert from buffer back to mp3 file 
 				 */
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
-		}
-	}
+		} 
+		
+ 	}
 }
-
